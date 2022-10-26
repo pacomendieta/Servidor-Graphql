8863d73cae58471690a735951326367054b56661
josejfml <jfml2014@gmail.com>
2022-10-26 19:02:31 +0200

    asus 26oct

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
 const express=require('express')
 const { GraphQLSchema, GraphQLObjectType, GraphQLString, graphql} = require('graphql')
+var { buildSchema } = require('graphql');
+var { graphqlHTTP } = require('express-graphql');
 
 const app = express();
 
-
-//SERVIDOR HTTP
-app.listen(process.env.puerto, ()=>{
-    console.log("Servidor http en puerto ", process.env.puerto)
-})
 
 //CREAR SQUEMA 
 const schema =  new GraphQLSchema({
@@ -24,20 +21,54 @@ const schema =  new GraphQLSchema({
     })
 })
 
-const mischema = buildSchema(`
-    type Query {
-        saludo:String
-    }
-`)
+
 const root = { 
-    saludo: ()=>{ console.log('Hola MUndo')}
+    saludo:   ()=>{ console.log('Hola MUndo')},
+    clientes: ()=>{ return clientes },
+    addCliente: (datos)=>{
+        var cli = {id:counter, nombre: datos.nombre, telefono:datos.telefono }
+        clientes.push( cli )
+        counter++
+        return cli
+    }
 }
 
 
 // RUTA / 
 app.get ('/',function(req,res) {
-    graphql( mischema, ` { message } `)
-    .then (respuesta=>res.json(respuesta))
-    .catch(error=>console.log(error) )
-    res.send("<h1>Pagina Home</h1>")
+    //graphql( mischema, ` { message } `)
+    //.then (respuesta=>res.json(respuesta))
+    //.catch(error=>console.log(error) )
+    res.render("paghome")
 }  )
+
+let clientes=[];
+let counter=1;
+let schemaClientes = buildSchema(`
+    type Cliente {
+        id: Int
+        nombre: String
+        telefono: String
+    }
+    type Query {
+        clientes: [Cliente]
+        cliente(id:Int):Cliente
+    }
+    type Mutation {
+        addCliente(id:Int, nombre:String, telefono:String ):Cliente
+    }
+`)
+
+app.use('/graphql', graphqlHTTP({
+    schema: schemaClientes,
+    rootValue: root,
+    graphiql: true,
+}));
+ 
+
+
+//SERVIDOR HTTP
+app.listen(process.env.puerto, ()=>{
+    console.log("Servidor http en puerto ", process.env.puerto)
+})
+app.set('view engine', 'ejs');
